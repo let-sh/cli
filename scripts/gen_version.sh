83895df4b7f772b6a581d46372b645ff27a2291c
Fred Liang <info@fredliang.cn>
2021-01-14 01:06:50 +0800

    fix: fix version generator

--- scripts/gen_version.sh
+++ scripts/gen_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-latest=$(git describe --match "v[0-9]*[0-9]" --exclude "*rc*" --exclude "*beta*"  --exclude "*-*" --abbrev=0 HEAD | sed 's/v//')
-beta=$(git describe --match "v[0-9]*beta" --exclude "*-*-*" --abbrev=0 HEAD | sed 's/v//')
-rc=$(git describe --match "v[0-9]*rc[0-9]" --abbrev=0 | sed 's/v//')
+latest=$(git describe --tags --match "v[0-9]*[0-9]" --exclude "*rc*" --exclude "*beta*" --abbrev=0 HEAD | sed 's/v//')
+beta=$(git describe --tags --match "v[0-9]*beta" --exclude "*-*-*" --abbrev=0 HEAD | sed 's/v//')
+rc=$(git describe --tags --match "v[0-9]*rc[0-9]" --abbrev=0 | sed 's/v//')
 echo -e "latest:$latest\nbeta:$beta\nrc:$rc" > version
