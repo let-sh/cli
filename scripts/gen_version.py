08698bb1e294c2a43c80473709d30f03e97ed21a
Fred Liang <info@fredliang.cn>
2021-09-20 22:52:05 +0800

    feat: change to py script

---
+++ scripts/gen_version.py
@@ -0,0 +1,10 @@
+import git
+import os
+repo = git.Repo(os.getcwd())
+tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
+tags = [str(tag) for tag in tags]
+latest_tag = tags[-1]
+beta_tag = [tag for tag in list(tags) if ("beta" in tag)][-1]
+rc_tag = [tag for tag in list(tags) if ("rc" in tag)][-1]
+with open('version', 'w') as f:
+    f.write(f'latest:{latest_tag}\nbeta:{beta_tag}\nrc:{rc_tag}')
