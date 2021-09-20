ed6aec780cc090b1253ca30ff5b43dec5ecc8440
Fred Liang <info@fredliang.cn>
2021-09-20 22:56:44 +0800

    fix: fix version tag error

--- scripts/gen_version.py
+++ scripts/gen_version.py
@@ -3,8 +3,8 @@ import os
 repo = git.Repo(os.getcwd())
 tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
 tags = [str(tag) for tag in tags]
-latest_tag = tags[-1]
-beta_tag = [tag for tag in list(tags) if ("beta" in tag)][-1]
-rc_tag = [tag for tag in list(tags) if ("rc" in tag)][-1]
+latest_tag = tags[-1][1:]
+beta_tag = [tag for tag in list(tags) if ("beta" in tag)][-1][1:]
+rc_tag = [tag for tag in list(tags) if ("rc" in tag)][-1][1:]
 with open('version', 'w') as f:
     f.write(f'latest:{latest_tag}\nbeta:{beta_tag}\nrc:{rc_tag}')
